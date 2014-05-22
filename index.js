9a94ead5039c3b32c246472af0be59086aace705
droppedonjapan <dropped.on.japan@gmail.com>
2014-05-22 15:17:26 -0500

    Unified code indention.
    As discussed in jwerle/to-csv#2

--- index.js
+++ index.js
@@ -14,29 +14,29 @@ CSV.CHAR_ENCAPSULATE = 0x22;
 
 
 function head (a) {
-	return a[0];
+  return a[0];
 }
 
 function tail (a) {
-	return a[a.length -1];
+  return a[a.length -1];
 }
 
 function char (c) {
-	return 'number' === typeof c
-	         ? String.fromCharCode.apply(null, arguments)
-	         : c;
+  return 'number' === typeof c
+           ? String.fromCharCode.apply(null, arguments)
+           : c;
 }
 
 function needsEncapsulation (string) {
-	return string.toString().indexOf(char(CSV.DELIMITER)) >= 0;
+  return string.toString().indexOf(char(CSV.DELIMITER)) >= 0;
 }
 
 function encapsulate (string, wrapperChar) {
-	var replaceWith = '\\' + wrapperChar;
-	var escapedValue = string.toString()
-													 .replace(new RegExp(wrapperChar, "g"), replaceWith);
+  var replaceWith = '\\' + wrapperChar;
+  var escapedValue = string.toString()
+                           .replace(new RegExp(wrapperChar, "g"), replaceWith);
 
-	return wrapperChar + escapedValue + wrapperChar;
+  return wrapperChar + escapedValue + wrapperChar;
 }
 
 /**
@@ -45,42 +45,42 @@ function encapsulate (string, wrapperChar) {
 
 module.exports = CSV;
 function CSV (objects, opts) {
-	if ('object' !== typeof objects) throw new TypeError("expecting an array");
+  if ('object' !== typeof objects) throw new TypeError("expecting an array");
 
-	opts = 'object' === typeof opts
-	        ? opts
-	        : {};
+  opts = 'object' === typeof opts
+          ? opts
+          : {};
 
-	objects = isArray(objects)
-	           ? objects
-	           : [objects];
+  objects = isArray(objects)
+             ? objects
+             : [objects];
 
-	if (!objects.length) throw new Error("expecting at least one object");
+  if (!objects.length) throw new Error("expecting at least one object");
 
-	var headers = keys(head(objects))
-	  , buf = [];
+  var headers = keys(head(objects))
+    , buf = [];
 
-	while (objects.length) {
-		var lbuf = []
-		  , object = objects.shift();
+  while (objects.length) {
+    var lbuf = []
+      , object = objects.shift();
 
-		for (var i = 0 ;i < headers.length; ++i) {
-			var header = headers[i];
+    for (var i = 0 ;i < headers.length; ++i) {
+      var header = headers[i];
 
-			if (lbuf.length) lbuf.push(char(CSV.DELIMITER));
-			object[header] = needsEncapsulation(object[header])
-												? encapsulate(object[header], char(CSV.CHAR_ENCAPSULATE))
-												: object[header];
+      if (lbuf.length) lbuf.push(char(CSV.DELIMITER));
+      object[header] = needsEncapsulation(object[header])
+                        ? encapsulate(object[header], char(CSV.CHAR_ENCAPSULATE))
+                        : object[header];
 
 
-			lbuf.push(object[header]);
-		}
+      lbuf.push(object[header]);
+    }
 
-		buf.push(lbuf.join(''));
-		buf.push(char(CSV.CHAR_RETURN, CSV.CHAR_NEWLINE));
-	}
+    buf.push(lbuf.join(''));
+    buf.push(char(CSV.CHAR_RETURN, CSV.CHAR_NEWLINE));
+  }
 
-	return false !== opts.headers
-	        ? [].concat(headers.join(','), char(CSV.CHAR_NEWLINE)).concat(buf).join('')
-	        : buf.join('');
+  return false !== opts.headers
+          ? [].concat(headers.join(','), char(CSV.CHAR_NEWLINE)).concat(buf).join('')
+          : buf.join('');
 }
